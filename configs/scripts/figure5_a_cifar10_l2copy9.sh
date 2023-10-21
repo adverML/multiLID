53fa68d364ceddca7d95cc045fbf54d2ffd82c04
Peter Lorenz <peter.lorenz@itwm.fraunhofer.de>
2023-10-21 17:26:15 +0200

    .

---
+++ configs/scripts/figure5_a_cifar10_l2copy9.sh
@@ -0,0 +1,65 @@
+
+# python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.5.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.5.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.5.json --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.5.json --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.5.json --run_nr run_2
+
+# python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.4.json   --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.4.json   --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.4.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.4.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.4.json  --run_nr run_2
+
+# python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.3.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.3.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.3.json --run_nr run_2 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.3.json --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.3.json --run_nr run_2 
+
+# python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.2.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.2.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.2.json --run_nr run_2 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.2.json --run_nr run_2 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.2.json --run_nr run_2 
+
+# python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.1.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.1.json  --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.1.json --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.1.json --run_nr run_2
+# python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.1.json --run_nr run_2
+
+
+
+
+
+
+# python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.5.json  --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.5.json  --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.5.json --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.5.json --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.5.json --run_nr run_3 
+
+# python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.4.json   --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.4.json   --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.4.json  --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.4.json  --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.4.json  --run_nr run_3 
+
+# python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.3.json  --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.3.json  --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.3.json --run_nr run_3  
+# python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.3.json --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.3.json --run_nr run_3  
+
+python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.2.json  --run_nr run_3 
+python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.2.json  --run_nr run_3 
+python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.2.json --run_nr run_3  
+python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.2.json --run_nr run_3  
+python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.2.json --run_nr run_3  
+
+# python extract.py --load_json cifar10/wrn28-10/multilid/k3/apgd-ce_l2_0.1.json  --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k5/apgd-ce_l2_0.1.json  --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k10/apgd-ce_l2_0.1.json --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k30/apgd-ce_l2_0.1.json --run_nr run_3 
+# python extract.py --load_json cifar10/wrn28-10/multilid/k50/apgd-ce_l2_0.1.json --run_nr run_3 
---
+++ configs/scripts/imagenet/wrn50-2-tmp-Copy1.sh
@@ -0,0 +1,3 @@
+python extract.py --load_json imagenet/wrn50-2/lid/k5/apgd-ce_l2_0.1.json --run_nr run_3
+python extract.py --load_json imagenet/wrn50-2/lid/k10/apgd-ce_l2_0.1.json --run_nr run_3
+
